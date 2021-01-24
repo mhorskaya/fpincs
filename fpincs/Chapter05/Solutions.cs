72015ab2615dc2c9b392e969af11cef2f4106f74
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2021-01-24 23:07:32 +0300

    Add solutions for chapter 05

---
+++ fpincs/Chapter05/Solutions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace fpincs.Chapter05
+{
+    internal static class Solutions
+    {
+        private static Func<T1, R> Compose<T1, T2, R>(this Func<T2, R> g, Func<T1, T2> f)
+            => x => g(f(x));
+    }
+}
